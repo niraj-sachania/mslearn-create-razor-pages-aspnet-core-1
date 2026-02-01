f5f0388c2355f2015902daf69daa70cdfea062a4
niraj-sachania <>
2026-02-01 04:32:57 +0000

    feat: implement NewPizza form and handling in PizzaList page

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -19,5 +19,20 @@ namespace ContosoPizza.Pages
         {
             PizzaList = _service.GetPizzas();
         }
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
     }
 }
